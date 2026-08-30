b6855b282a5695c83df570487a8bf2d7753a31da
DarkFox <darkfox.tobias@outlook.com>
2026-08-30 12:55:16 +0200

    Add hw types.h

---
+++ include/hw/types.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+typedef volatile uint32_t vu32;
+typedef volatile uint16_t vu16;
+typedef volatile uint8_t  vu8;
+
+typedef uint32_t u32;
+typedef uint16_t u16;
+typedef uint8_t  u8;
+typedef int32_t  s32;
+typedef int16_t  s16;
+typedef int8_t   s8;
