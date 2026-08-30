b4a66e2cdfe7912297fa35f8ffe7aa475f9b6883
DarkFox <darkfox.tobias@outlook.com>
2026-08-30 10:40:12 +0200

    Update main.c with all modules + splash

--- source/main.c
+++ source/main.c
@@ -11,6 +11,8 @@
 #include "fs.h"
 #include "net.h"
 #include "o10.h"
+#include "sys.h"
+#include "splash.h"
 
 int main(int argc, char** argv)
 {
@@ -18,26 +20,36 @@ int main(int argc, char** argv)
     (void)argv;
 
     // Initialize core systems
+    sys_init();
     ui_init();
     fs_init();
     net_init();
     o10_init();
+    splash_init();
 
-    // TODO: Show splash screen from assets/splash/
-    // TODO: Mount SD + NAND
-    // TODO: Start main UI loop or script runner
+    // Show splash screen
+    splash_show();
+
+    // TODO: Mount SD + NAND partitions
+    // TODO: Check for autorun script (data/autorun.o10 or autorun.lua)
+    // TODO: Start main file browser / UI loop
 
     ui_echo("Omni10-3DS " OMNI10_VERSION_STRING);
     ui_echo("Full Access. No Limits.");
 
-    // Placeholder – real main loop comes later
-    // while (true) { ui_handle_input(); ... }
+    // Main loop placeholder
+    // while (running) {
+    //     ui_handle_input();
+    //     // file browser, scripts menu, etc.
+    // }
 
     // Cleanup
+    splash_deinit();
     o10_deinit();
     net_deinit();
     fs_deinit();
     ui_deinit();
+    sys_deinit();
 
     return 0;
 }
