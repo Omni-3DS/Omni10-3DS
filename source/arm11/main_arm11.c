ebdf719099f423cbdf4e6c88bb0bfbb159042f1a
DarkFox <darkfox.tobias@outlook.com>
2026-08-30 12:52:27 +0200

    Add ARM11 main (own)

---
+++ source/arm11/main_arm11.c
@@ -0,0 +1,22 @@
+/*
+ * Omni10-3DS – ARM11 main
+ * UI, input, networking/FTP, splash
+ * Original code – not derived from GodMode9
+ */
+
+#include "common.h"
+
+void main_arm11(void)
+{
+    /* TODO:
+     * 1. Init GPU / framebuffers
+     * 2. Show splash
+     * 3. Input
+     * 4. UI loop
+     * 5. FTP server (sockets live here)
+     */
+
+    while (1) {
+        /* UI / net loop */
+    }
+}
