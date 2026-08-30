3caffce213531555aea182054bddea83809001ee
DarkFox <darkfox.tobias@outlook.com>
2026-08-30 10:35:43 +0200

    Add main entry point skeleton

---
+++ source/main.c
@@ -0,0 +1,33 @@
+/*
+ * Omni10-3DS
+ * Main entry point (placeholder)
+ *
+ * Copyright (C) 2026 Omni10 Team
+ * Licensed under GPL-3.0
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+// Placeholder includes – will be replaced with real headers later
+// #include "common.h"
+// #include "ui.h"
+// #include "fs.h"
+// #include "lua_api.h"
+// #include "o10.h"
+
+int main(int argc, char** argv)
+{
+    (void)argc;
+    (void)argv;
+
+    // TODO: Initialize hardware
+    // TODO: Show splash screen
+    // TODO: Mount filesystems
+    // TODO: Start UI / script runner
+
+    // Placeholder message (will be replaced by real framebuffer output)
+    // For now this file just exists so the project structure is ready.
+
+    return 0;
+}
