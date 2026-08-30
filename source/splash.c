b11e341da2dc835b8524d2ced4c176562a41eee2
DarkFox <darkfox.tobias@outlook.com>
2026-08-30 12:08:51 +0200

    Update splash.c for final design

--- source/splash.c
+++ source/splash.c
@@ -1,12 +1,18 @@
 /*
- * Omni10-3DS – Splash Screen (stub)
+ * Omni10-3DS – Splash Screen
+ *
+ * Official design:
+ * - Pure black background
+ * - Bold pixel-style white text "Omni10-3DS"
+ * - Gray subtitle "Full Access. No Limits."
+ * - Classic GodMode9 aesthetic
  */
 
 #include "splash.h"
 
 omni_result_t splash_init(void)
 {
-    // TODO: Load splash image from data/ or assets/
+    // TODO: Load splash.png from data/ or embedded VRAM
     return OMNI_OK;
 }
 
@@ -16,8 +22,12 @@ void splash_deinit(void)
 
 omni_result_t splash_show(void)
 {
-    // TODO: Draw splash on top (and optional bottom) screen
-    // TODO: Wait for key or timeout
+    // TODO:
+    // 1. Clear screens to black
+    // 2. Draw splash.png centered on top screen (400x240)
+    // 3. Optionally draw bottom screen content
+    // 4. Wait ~1 second or until key press (like GodMode9)
+
     return OMNI_OK;
 }
 
