b0ee8b938f0180637624b153de891fec5d319d97
DarkFox <darkfox.tobias@outlook.com>
2026-08-31 13:36:29 +0200

    Harden start.s: save argc/argv, set SP from linker

--- firm/arm9/start.s
+++ firm/arm9/start.s
@@ -1,17 +1,30 @@
-/* Omni10 ARM9 entry – set stack, forward argc/argv from Luma chainloader */
+/*
+ * Omni10 ARM9 entry
+ * Luma chainloader jumps here with:
+ *   r0 = argc
+ *   r1 = argv
+ */
 
-.section .text.startup
+.section .text.startup, "ax", %progbits
 .global _start
+.type _start, %function
 .align 4
 
 _start:
-    ldr sp, =stack_top
-    /* r0 = argc, r1 = argv (from Luma) */
-    bl  main
-1:
-    b   1b
+    /* Stack at end of our image RAM */
+    ldr sp, =_stack_top
 
+    /* Keep argc/argv for main (r0, r1 already set by Luma) */
+    bl  main
+
+.hang:
+    b   .hang
+
+.size _start, . - _start
+
+/* 8KB stack in BSS */
 .section .bss
-.align 4
+.align 3
 .space 0x2000
-stack_top:
+.global _stack_top
+_stack_top:
