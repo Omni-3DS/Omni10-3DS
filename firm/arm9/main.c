29b48a6dee50177c7d01e433584cf36f41bd3a33
DarkFox <darkfox.tobias@outlook.com>
2026-08-31 14:40:37 +0200

    RGB888 match Luma FB + PIXEL_OFFSET

--- firm/arm9/main.c
+++ firm/arm9/main.c
@@ -1,5 +1,6 @@
 /*
- * Omni10 – draw + START+SELECT power off
+ * Omni10 ARM9
+ * Luma FB: RGB888, PIXEL_OFFSET(x,y) = x*240 + (239-y)
  */
 
 #include <stdint.h>
@@ -11,32 +12,27 @@
 #define SCREEN_W 400
 #define SCREEN_H 240
 
-#define FB_TOP0 ((volatile uint16_t *)0x18300000)
-#define FB_TOP1 ((volatile uint16_t *)0x18400000)
-#define FB_BOT0 ((volatile uint16_t *)0x18346500)
-#define FB_BOT1 ((volatile uint16_t *)0x18446500)
+#define FB_TOP0 ((volatile uint8_t *)0x18300000)
+#define FB_TOP1 ((volatile uint8_t *)0x18400000)
+#define FB_BOT0 ((volatile uint8_t *)0x18346500)
+#define FB_BOT1 ((volatile uint8_t *)0x18446500)
 
-#define BTN_A      (1u << 0)
-#define BTN_B      (1u << 1)
 #define BTN_SELECT (1u << 2)
 #define BTN_START  (1u << 3)
 #define BTN_RIGHT  (1u << 4)
 #define BTN_LEFT   (1u << 5)
 
-#define RGB565(r, g, b) ((uint16_t)(((r) & 0x1F) << 11 | ((g) & 0x3F) << 5 | ((b) & 0x1F)))
 #define PIXEL_OFFSET(x, y) (((x) * SCREEN_H) + (SCREEN_H - (y) - 1))
 
-/* ---- minimal I2C (MCU power off) ---- */
 #define I2C2_REGS_BASE 0x10144000
-
 #define I2C_ENABLE     (1u << 7)
 #define I2C_IRQ_ENABLE (1u << 6)
 #define I2C_START      (1u << 5)
 #define I2C_STOP       (1u << 4)
 #define I2C_ERROR      (1u << 1)
-#define I2C_ACK        (1u << 2)
 #define I2C_DIRE_WRITE 0
 #define I2C_GET_ACK(r) (((r) >> 4) & 1u)
+#define MCU_DEV_ADDR 0x4A
 
 typedef struct {
     volatile uint8_t  REG_I2C_DATA;
@@ -45,8 +41,24 @@ typedef struct {
     volatile uint16_t REG_I2C_SCL;
 } I2cRegs;
 
-/* MCU = device 3, bus 1, addr 0x4A */
-#define MCU_DEV_ADDR 0x4A
+struct fb {
+    uint8_t *top_left;
+    uint8_t *top_right;
+    uint8_t *bottom;
+};
+
+static volatile uint8_t *g_top[2];
+static volatile uint8_t *g_bot[2];
+static int g_n_top;
+static int g_n_bot;
+
+void *memset(void *dest, int val, size_t count)
+{
+    uint8_t *p = (uint8_t *)dest;
+    while (count--)
+        *p++ = (uint8_t)val;
+    return dest;
+}
 
 static void i2c_wait(I2cRegs *regs)
 {
@@ -57,9 +69,7 @@ static void i2c_wait(I2cRegs *regs)
 static bool i2c_write_mcu(uint8_t reg, uint8_t data)
 {
     I2cRegs *regs = (I2cRegs *)I2C2_REGS_BASE;
-    int tries;
-
-    for (tries = 0; tries < 8; tries++) {
+    for (int tries = 0; tries < 8; tries++) {
         i2c_wait(regs);
         regs->REG_I2C_DATA = MCU_DEV_ADDR;
         regs->REG_I2C_CNT = I2C_ENABLE | I2C_IRQ_ENABLE | I2C_START;
@@ -68,7 +78,6 @@ static bool i2c_write_mcu(uint8_t reg, uint8_t data)
             regs->REG_I2C_CNT = I2C_ENABLE | I2C_IRQ_ENABLE | I2C_ERROR | I2C_STOP;
             continue;
         }
-
         regs->REG_I2C_DATA = reg;
         regs->REG_I2C_CNT = I2C_ENABLE | I2C_IRQ_ENABLE | I2C_DIRE_WRITE;
         i2c_wait(regs);
@@ -76,7 +85,6 @@ static bool i2c_write_mcu(uint8_t reg, uint8_t data)
             regs->REG_I2C_CNT = I2C_ENABLE | I2C_IRQ_ENABLE | I2C_ERROR | I2C_STOP;
             continue;
         }
-
         regs->REG_I2C_DATA = data;
         regs->REG_I2C_CNT = I2C_ENABLE | I2C_IRQ_ENABLE | I2C_DIRE_WRITE | I2C_STOP;
         i2c_wait(regs);
@@ -91,30 +99,9 @@ static bool i2c_write_mcu(uint8_t reg, uint8_t data)
 
 static void power_off(void)
 {
-    /* MCU reg 0x20 bit0 = power off */
     i2c_write_mcu(0x20, 1);
     while (1)
         ;
-}
-
-/* ---- FB ---- */
-struct fb {
-    uint8_t *top_left;
-    uint8_t *top_right;
-    uint8_t *bottom;
-};
-
-static volatile uint16_t *g_top[2];
-static volatile uint16_t *g_bot[2];
-static int g_n_top;
-static int g_n_bot;
-
-void *memset(void *dest, int val, size_t count)
-{
-    uint8_t *p = (uint8_t *)dest;
-    while (count--)
-        *p++ = (uint8_t)val;
-    return dest;
 }
 
 static void drain(void)
@@ -129,46 +116,41 @@ static void delay(int n)
         __asm__ volatile("nop");
 }
 
-static void put(int x, int y, uint16_t c)
+static void put(int x, int y, uint8_t r, uint8_t g, uint8_t b)
 {
     if (x < 0 || y < 0 || x >= SCREEN_W || y >= SCREEN_H)
         return;
-    int idx = PIXEL_OFFSET(x, y);
-    for (int i = 0; i < g_n_top; i++)
-        if (g_top[i])
-            g_top[i][idx] = c;
-}
-
-static void clear_top(uint16_t color)
-{
-    uint32_t c32 = ((uint32_t)color << 16) | color;
-    int n = (SCREEN_W * SCREEN_H) / 2;
-    for (int t = 0; t < g_n_top; t++) {
-        if (!g_top[t])
+    int idx = PIXEL_OFFSET(x, y) * 3;
+    for (int i = 0; i < g_n_top; i++) {
+        if (!g_top[i])
             continue;
-        volatile uint32_t *p = (volatile uint32_t *)g_top[t];
-        for (int i = 0; i < n; i++)
-            p[i] = c32;
+        g_top[i][idx + 0] = b;
+        g_top[i][idx + 1] = g;
+        g_top[i][idx + 2] = r;
     }
 }
 
-static void rect(int x, int y, int w, int h, uint16_t c)
+static void clear_top(uint8_t r, uint8_t g, uint8_t b)
+{
+    for (int x = 0; x < SCREEN_W; x++)
+        for (int y = 0; y < SCREEN_H; y++)
+            put(x, y, r, g, b);
+}
+
+static void rect(int x, int y, int w, int h, uint8_t r, uint8_t g, uint8_t b)
 {
     for (int j = 0; j < h; j++)
         for (int i = 0; i < w; i++)
-            put(x + i, y + j, c);
+            put(x + i, y + j, r, g, b);
 }
 
-static void clear_bot(uint16_t color)
+static void clear_bot(void)
 {
-    uint32_t c32 = ((uint32_t)color << 16) | color;
-    int n = (240 * 320) / 2;
     for (int t = 0; t < g_n_bot; t++) {
         if (!g_bot[t])
             continue;
-        volatile uint32_t *p = (volatile uint32_t *)g_bot[t];
-        for (int i = 0; i < n; i++)
-            p[i] = c32;
+        for (int i = 0; i < 320 * 240 * 3; i++)
+            g_bot[t][i] = 0;
     }
 }
 
@@ -186,13 +168,13 @@ int main(int argc, char **argv)
         g_n_top = 0;
         g_n_bot = 0;
         if (fbs[0].top_left)
-            g_top[g_n_top++] = (volatile uint16_t *)fbs[0].top_left;
+            g_top[g_n_top++] = (volatile uint8_t *)fbs[0].top_left;
         if (fbs[1].top_left)
-            g_top[g_n_top++] = (volatile uint16_t *)fbs[1].top_left;
+            g_top[g_n_top++] = (volatile uint8_t *)fbs[1].top_left;
         if (fbs[0].bottom)
-            g_bot[g_n_bot++] = (volatile uint16_t *)fbs[0].bottom;
+            g_bot[g_n_bot++] = (volatile uint8_t *)fbs[0].bottom;
         if (fbs[1].bottom)
-            g_bot[g_n_bot++] = (volatile uint16_t *)fbs[1].bottom;
+            g_bot[g_n_bot++] = (volatile uint8_t *)fbs[1].bottom;
         if (g_n_top == 0) {
             g_top[0] = FB_TOP0;
             g_top[1] = FB_TOP1;
@@ -200,19 +182,12 @@ int main(int argc, char **argv)
         }
     }
 
-    const uint16_t BLK = RGB565(0, 0, 0);
-    const uint16_t RED = RGB565(31, 0, 0);
-    const uint16_t GRN = RGB565(0, 63, 0);
-    const uint16_t BLU = RGB565(0, 0, 31);
-    const uint16_t WHT = RGB565(31, 63, 31);
-    const uint16_t CYN = RGB565(0, 55, 31);
-
-    clear_top(BLK);
-    rect(0, 0, 200, 120, RED);
-    rect(200, 0, 200, 120, GRN);
-    rect(0, 120, 200, 120, BLU);
-    rect(200, 120, 200, 120, WHT);
-    clear_bot(BLK);
+    clear_top(0, 0, 0);
+    rect(0, 0, 200, 120, 255, 0, 0);
+    rect(200, 0, 200, 120, 0, 255, 0);
+    rect(0, 120, 200, 120, 0, 0, 255);
+    rect(200, 120, 200, 120, 255, 255, 255);
+    clear_bot();
     drain();
 
     int bar_x = 160;
@@ -227,30 +202,30 @@ int main(int argc, char **argv)
                 for (int i = 0; i < bar_w; i++) {
                     int px = prev_x + i;
                     int py = bar_y + j;
-                    uint16_t under;
-                    if (px < 200 && py < 120)
-                        under = RED;
-                    else if (px >= 200 && py < 120)
-                        under = GRN;
-                    else if (px < 200)
-                        under = BLU;
-                    else
-                        under = WHT;
-                    put(px, py, under);
+                    uint8_t r, g, b;
+                    if (px < 200 && py < 120) {
+                        r = 255; g = 0; b = 0;
+                    } else if (px >= 200 && py < 120) {
+                        r = 0; g = 255; b = 0;
+                    } else if (px < 200) {
+                        r = 0; g = 0; b = 255;
+                    } else {
+                        r = 255; g = 255; b = 255;
+                    }
+                    put(px, py, r, g, b);
                 }
             }
         }
 
-        rect(bar_x, bar_y, bar_w, bar_h, CYN);
+        rect(bar_x, bar_y, bar_w, bar_h, 0, 255, 255);
         prev_x = bar_x;
         drain();
 
         uint32_t k = ~REG_PAD_HID;
 
-        /* START + SELECT → power off */
         if ((k & BTN_START) && (k & BTN_SELECT)) {
-            clear_top(BLK);
-            clear_bot(BLK);
+            clear_top(0, 0, 0);
+            clear_bot();
             drain();
             power_off();
         }
